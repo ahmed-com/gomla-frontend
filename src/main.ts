c1c64fa8a194243dfc55f8a6f15c3996e9a483f0
ahmed-com <ahmed0grwan@gmail.com>
2022-07-18 11:51:13 +0200

    added pinia to the project

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,16 @@
 import { createApp } from 'vue';
+import { createPinia } from 'pinia';
 import App from './App.vue';
 import vuetify from './plugins/vuetify';
 import { loadFonts } from './plugins/webfontloader';
 import router from './router';
 import i18n from './plugins/i18n';
 
+const pinia = createPinia();
 loadFonts();
 
 createApp(App)
+  .use(pinia)
   .use(i18n)
   .use(vuetify)
   .use(router)
