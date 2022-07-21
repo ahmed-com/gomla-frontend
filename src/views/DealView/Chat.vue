a307d0209752a52d7e28038f4036b560b4640e44
ahmed-com <ahmed0grwan@gmail.com>
2022-07-21 18:48:06 +0200

    just renamed a utility class

--- src/views/DealView/Chat.vue
+++ src/views/DealView/Chat.vue
@@ -12,22 +12,6 @@
         <div class="pa-4 ma-4 bg-blue">1</div>
         <div class="pa-4 ma-4 bg-blue">2</div>
         <div class="pa-4 ma-4 bg-blue">3</div>
-        <div class="pa-4 ma-4 bg-blue">4</div>
-        <div class="pa-4 ma-4 bg-blue">5</div>
-        <div class="pa-4 ma-4 bg-blue">6</div>
-        <div class="pa-4 ma-4 bg-blue">7</div>
-        <div class="pa-4 ma-4 bg-blue">8</div>
-        <div class="pa-4 ma-4 bg-blue">9</div>
-        <div class="pa-4 ma-4 bg-blue">10</div>
-        <div class="pa-4 ma-4 bg-blue">11</div>
-        <div class="pa-4 ma-4 bg-blue">12</div>
-        <div class="pa-4 ma-4 bg-blue">13</div>
-        <div class="pa-4 ma-4 bg-blue">14</div>
-        <div class="pa-4 ma-4 bg-blue">15</div>
-        <div class="pa-4 ma-4 bg-blue">16</div>
-        <div class="pa-4 ma-4 bg-blue">17</div>
-        <div class="pa-4 ma-4 bg-blue">18</div>
-        <div class="pa-4 ma-4 bg-blue">19</div>
         <chat-footer></chat-footer>
     </div>
 </template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,7 +5,7 @@
 </script>
 
 <template>
-	<div class="background-texture h-100 flex-grow-1">
+	<div class="bg-texture h-100 flex-grow-1">
 		{{t('homeMsg')}}
 		<br>
 		<router-link :to="{ name: 'Search' }">go to search</router-link>
