79f39820614fa2d2b0499456b36aefba42a73d6d
ahmed-com <ahmed0grwan@gmail.com>
2022-07-21 18:36:20 +0200

    added a utility class

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -9,22 +9,10 @@
 		{{t('homeMsg')}}
 		<br>
 		<router-link :to="{ name: 'Search' }">go to search</router-link>
-		<v-btn @click="toggleLocales">Toggle</v-btn>
 	</div>
 </template>
 
 <script setup lang="ts">
 	import { useI18n } from 'vue-i18n';
-	import { useRtl } from 'vuetify/lib/framework.mjs';
-	const x = useRtl();
-	const z = useI18n();
-	const { t } = z;
-	
-	const toggleLocales = ()=> z.locale.value = z.locale.value === "en" ? "ar" : "en"
+	const {t}= useI18n();
 </script>
-
-<style>
-	.background-texture{
-		background-image: var(--v-texture);
-	}
-</style>
