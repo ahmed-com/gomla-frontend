cfcc964de4b22e159f0e7ba9d4613e0aa4e9563d
ahmed-com <ahmed0grwan@gmail.com>
2022-08-09 07:21:40 +0200

    fixed the problem in the scrolling of the chat

--- src/components/NewMessagesDivider.vue
+++ src/components/NewMessagesDivider.vue
@@ -5,22 +5,15 @@
 </script>
 
 <template>
-    <div class="d-flex align-center" ref="divider">
-    <template v-if="showDivider">
+    <div class="d-flex align-center" >
         <v-divider></v-divider>
         <div class="bg-info rounded-pill pa-2 mx-2 flex-grow-0 flex-shrink-0 "> {{t('components.NewMessagesDivider.msg')}} </div>
         <v-divider></v-divider>
-    </template>
     </div>
 </template>
 
 <script setup lang="ts">
-import { onMounted, ref } from 'vue';
 import { useI18n } from 'vue-i18n';
 
 const { t } = useI18n();
-const divider = ref<HTMLElement | null>(null);
-const { showDivider } = defineProps<{showDivider: boolean}>();
-
-onMounted(()=>divider.value!.scrollIntoView());
 </script>
--- src/views/DealView/Chat.vue
+++ src/views/DealView/Chat.vue
@@ -8,6 +8,7 @@
 	<div ref="chatContainer" class="padding-for-footer max-screen overflow-y-auto scrollbar-hidden">
 		the deal chat
 		<router-link :to="{ name: 'Home' }">go to home</router-link>
+		<div ref="messagesContainer">
 			<div
 				v-for="(message, i) in messages"
 				:key="message.id"
@@ -19,45 +20,37 @@
 				<br v-if="message.attachment">
 				<v-img :eager="true" v-if="message.attachment" :src="message.attachment"></v-img>
 			</div>
-			<new-messages-divider :showDivider="true"></new-messages-divider>
+		</div>
+		<new-messages-divider></new-messages-divider>
 		<scroll-btn :showFAB="showFAB" @click="scrollToBottom('smooth')"></scroll-btn>
 		<chat-footer></chat-footer>
 	</div>
 </template>
 
 <script setup lang="ts">
-	import { computed, onMounted, onUnmounted, ref } from 'vue';
+	import { computed, ref } from 'vue';
     import ChatFooter from '../../components/ChatFooter.vue';
 	import { useMessages } from '../../stores/useMessages';
     import useLayoutManager from '../../composables/useLayoutManager';
 	import ScrollBtn from '../../components/ScrollBtn.vue';
 	import NewMessagesDivider from '../../components/NewMessagesDivider.vue';
+	import { useResizeObserver } from '@vueuse/core';
 
 	const { messages } = useMessages();
     const { headerSize } = useLayoutManager()
-
+	
+	const messagesContainer = ref<HTMLElement | null>(null);
+	const chatContainer = ref<HTMLElement | null>(null);
 	const showFAB = ref<boolean>(false);
+
     const computedHeaderSize = computed(()=> `${headerSize.value}px`);
 	const scrollToBottom = (behavior: ScrollBehavior)=> chatContainer.value!.scrollTo({
 		top: chatContainer.value!.scrollHeight,
 		behavior
 	});
-
-    const mutationCallback: MutationCallback = (_: MutationRecord[]) => !showFAB.value && scrollToBottom('smooth');
-    const mutationObserver = new MutationObserver(mutationCallback) 
-
-    const chatContainer = ref<HTMLElement | null>(null)
-
-    onMounted(()=>{
-        mutationObserver.observe(chatContainer.value!,{
-            childList: true,
-            subtree: true
-        })
-    });
-
-	onUnmounted(()=>mutationObserver.disconnect());
-
 	const onIntersect = (index: number) => (isIntersecting: boolean) => (index === messages.length - 1) ? showFAB.value = !isIntersecting : undefined;
+
+	useResizeObserver(messagesContainer,()=> !showFAB.value && scrollToBottom('smooth'))
 </script>
 
 <style scoped lang="scss">
